aa09351c6437424a8a3794406acd5bd25dd64cd4
Hugo Nicolás Leyton Díaz <hleyton@alumnos.inf.utfsm.cl>
2021-08-11 21:09:00 -0400

    data/data.json has been removed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React, { Component} from 'react';
-import './App.css';
 import { BrowserRouter as Router, Switch} from 'react-router-dom';
 import { DashboardRoutes } from './routers/DashboarRoutes';
-import data from './data/data.json';
 
 class App extends Component {
-  async componentDidMount() {
-    // localStorage.clear();
-    if (window.localStorage.encuestas === undefined) {
-      localStorage.setItem("encuestas", JSON.stringify(data));
-    }
-    // console.log(JSON.parse(localStorage.encuestas));
-  }
   render() {
     return (
       <Router>
